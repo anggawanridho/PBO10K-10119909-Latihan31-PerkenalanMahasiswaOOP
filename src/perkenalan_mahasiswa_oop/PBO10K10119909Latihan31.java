505a8c813a69c13ed0c350b7e0cf097ab62aadf2
anggawanridho <anggawanridho@gmail.com>
2020-11-03 13:57:47 +0700

    rearranged

--- src/perkenalan_mahasiswa_oop/PBO10K10119909Latihan31.java
+++ src/perkenalan_mahasiswa_oop/PBO10K10119909Latihan31.java
@@ -16,15 +16,15 @@ public class PBO10K10119909Latihan31 {
         Mahasiswa mhs4 = new Mahasiswa();
         mhs1.nim = "10110269";
         mhs1.nama = "Rizki Adam Kurniawan";
-        mhs1.pekenalkanDiri();
         mhs2.nim = "10112270";
         mhs2.nama = "Indra Tiola";
-        mhs2.pekenalkanDiri();
         mhs3.nim = "10110271";
         mhs3.nama = "Robi Tansil Ganefi";
-        mhs3.pekenalkanDiri();
         mhs4.nim = "10110269";
         mhs4.nama = "Muhammad Nur Awaludin";
+        mhs1.pekenalkanDiri();
+        mhs2.pekenalkanDiri();
+        mhs3.pekenalkanDiri();
         mhs4.pekenalkanDiri();
     }
 }
